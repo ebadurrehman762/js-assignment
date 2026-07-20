4419b9e50419c7aca74b727bdd84facc66f2765d
ebadurrehman762 <ebadurrehman762@gmail.com>
2026-07-20 21:17:45 +0500

    main

--- lesson 2/app.js
+++ lesson 2/app.js
@@ -1,113 +1,113 @@
-// var a = prompt("Enter a number");
-// if (a > 0) {
-//     console.log("Positive");
-// }else {
-//     console.log("Negative");
-// }
+var a = prompt("Enter a number");
+if (a > 0) {
+    console.log("Positive");
+}else {
+    console.log("Negative");
+}
 
-// var b = prompt("Enter a number");
-// if (b % 2 == 0) {
-//     console.log("Even");
-// }else {
-//     console.log("Odd");
-// }
+var b = prompt("Enter a number");
+if (b % 2 == 0) {
+    console.log("Even");
+}else {
+    console.log("Odd");
+}
 
-// var c = prompt("Enter your age");
-// if (c > 18) {
-//     console.log("eligible to vote");
-// }else {
-//     console.log("Not eligible to vote");
-// }
+var c = prompt("Enter your age");
+if (c > 18) {
+    console.log("eligible to vote");
+}else {
+    console.log("Not eligible to vote");
+}
 
-// var d = prompt("Enter your marks");
-// if (d >= 50) {
-//     console.log("Pass");
-// }else {
-//     console.log("Fail");
-// }
+var d = prompt("Enter your marks");
+if (d >= 50) {
+    console.log("Pass");
+}else {
+    console.log("Fail");
+}
 
-// var e = prompt("Enter a number");
-// if (e > 100) {
-//     console.log("Large number");
-// }else {
-//     console.log("Small number");
-// }
-
-
-// var f = prompt("Enter password");
-// if (f >= "admin123") {
-//     console.log("Login successful");
-// }else {
-//     console.log("incorrect password");
-// }
+var e = prompt("Enter a number");
+if (e > 100) {
+    console.log("Large number");
+}else {
+    console.log("Small number");
+}
 
 
-
-// var g = prompt("Enter your balance");
-// var h = prompt("Enter item price");
-// if (g >= h) {
-//     console.log("Purchase successful");
-// }else {
-//     console.log("Insufficient balance");
-// }
-
-
-// var i = prompt("Enter temperature");
-// if (i > 30) {
-//     console.log("it's hot");
-// }else {
-//     console.log("Weather is plaeasant");
-// }
-
-
-// var j = 20;
-// if (j % 5 == 0) {
-//     console.log("divide by 5");
-// }else {
-//     console.log("No divide by 5");
-// }
+var f = prompt("Enter password");
+if (f >= "admin123") {
+    console.log("Login successful");
+}else {
+    console.log("incorrect password");
+}
 
 
 
-// var k = prompt("traffic light color");
-// if (k == "green") {
-//     console.log("go");
-// } else {
-//     console.log("stop");
-// }
+var g = prompt("Enter your balance");
+var h = prompt("Enter item price");
+if (g >= h) {
+    console.log("Purchase successful");
+}else {
+    console.log("Insufficient balance");
+}
 
 
-// var l = prompt("username");
-// if (l == "admin") {
-//     console.log("welcome admin");
-// } else {
-//     console.log("Access denied");
-// }
+var i = prompt("Enter temperature");
+if (i > 30) {
+    console.log("it's hot");
+}else {
+    console.log("Weather is plaeasant");
+}
 
 
-// var m = prompt("enter your height");
-// if (m >= "170") {
-//     console.log("Tall");
-// } else {
-//     console.log("Average height");
-// }
-
-
-// var n = prompt("username");
-// if (n == "user") {
-//     console.log("welcome back");
-// } else {
-//     console.log("please login");
-// }
+var j = 20;
+if (j % 5 == 0) {
+    console.log("divide by 5");
+}else {
+    console.log("No divide by 5");
+}
 
 
 
-// var o = prompt("enter your age");
-// if (o >= "60") {
-//     console.log("senior citizen");
-// } else {
-//     console.log("not a senior citizen");
-// }
+var k = prompt("traffic light color");
+if (k == "green") {
+    console.log("go");
+} else {
+    console.log("stop");
+}
+
+
+var l = prompt("username");
+if (l == "admin") {
+    console.log("welcome admin");
+} else {
+    console.log("Access denied");
+}
+
+
+var m = prompt("enter your height");
+if (m >= "170") {
+    console.log("Tall");
+} else {
+    console.log("Average height");
+}
+
+
+var n = prompt("username");
+if (n == "user") {
+    console.log("welcome back");
+} else {
+    console.log("please login");
+}
+
+
+
+var o = prompt("enter your age");
+if (o >= "60") {
+    console.log("senior citizen");
+} else {
+    console.log("not a senior citizen");
+}
 
 
 
